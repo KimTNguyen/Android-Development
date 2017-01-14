5fccbd54661c86db358567d137d2d05634b0f6fb
KimTNguyen <kimthy1234@yahoo.com>
2017-01-13 22:01:45 -0800

    handle the case when rotating screen

--- DrugList/app/src/main/java/projects/android/nguyen/kim/druglist/MainActivity.java
+++ DrugList/app/src/main/java/projects/android/nguyen/kim/druglist/MainActivity.java
@@ -5,6 +5,7 @@
  * @author Kim Nguyen
  * @version 09-Jan-2017
  */
+
 package projects.android.nguyen.kim.druglist;
 
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +18,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -25,106 +27,27 @@ import java.util.Scanner;
 public class MainActivity extends AppCompatActivity {
 
     final int NO_GENERIC_ON_SCREEN = 5;
-    private Map<String,String> generatedDrugArray = new HashMap<>();
+
+    private Map<String, String> brandAndGeneric = new HashMap<>();
+    private Map<String,String> generatedDrugs = new HashMap<>();
+    private String brandName = null;
+    private ListView drugView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pickDrugName();
+        drugView = (ListView) findViewById(R.id.drug_list);
+
+        initDrugData();
+        generateListDrugs();
     }
 
     /**
-     * Sets text for the TextView with the random brand name
-     *
-     * @return the random brand name
+     * Creates a list of brand and it's generic
      */
-    protected String displayBrandName() {
-        Random brandPos = new Random();
-        TextView genericView = (TextView) findViewById(R.id.brand);
-
-        Log.d("nameList", generatedDrugArray.keySet().toString());
-
-        /* Generates a random number from the set of drug name */
-        String brandName = (String) generatedDrugArray.keySet().toArray()[brandPos.nextInt(NO_GENERIC_ON_SCREEN)];
-        Log.d("generatedBrad",brandName);
-
-        genericView.setText(brandName);
-        return brandName;
-    }
-
-    /**
-     * Gives feedback right or wrong when the user clicks on the generic name on the list
-     */
-    protected void pickDrugName() {
-        generatedDrugArray.clear();
-        generatedDrugArray = generateListDrugs();
-        final String nameGenerated = displayBrandName();
-        ListView drugView = (ListView) findViewById(R.id.drug_list);
-
-        Log.d("genericList", generatedDrugArray.values().toString());
-
-        /* Displays a list of random generic name on the screen */
-        ArrayAdapter<String> drugAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, generatedDrugArray.values().toArray(new String[0]));
-        drugView.setAdapter(drugAdapter);
-
-        drugView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int brand, long l) {
-                Log.d("position", "the user click on: " + brand);
-                Log.d("position", "the user click on: " + generatedDrugArray.values().toArray()[brand]);
-                Log.d("position", "the user click on: " + generatedDrugArray.get(nameGenerated));
-
-                if (generatedDrugArray.get(nameGenerated).equals(generatedDrugArray.values().toArray()[brand])) {
-                    Toast.makeText(MainActivity.this, "You are awesome!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "You suck!", Toast.LENGTH_SHORT).show();
-                }
-                pickDrugName();
-            }
-        });
-    }
-
-    /**
-     * Initializes the drug data in the form of Map data structure
-     *
-     * @return the map of drug's brand name and its generic
-     */
-    private Map<String, String> initDrugData() {
-
-        Map<String, String> drugData = new HashMap<>();
-//        drugData.put("z-pak", "azithromycin 250mg");
-//        drugData.put("tri-pak", "azithromycin 500mg");
-//        drugData.put("medrol", "methylprednisolone");
-//        drugData.put("soma", "carisoprodol");
-//        drugData.put("peridex", "chlorhexidine");
-//        drugData.put("microbid", "nitrofurantoin");
-//        drugData.put("amoxil", "amoxicillin");
-//        drugData.put("keflex", "cephalexcin");
-//        drugData.put("flonase", "fluticasone");
-//        drugData.put("cheratussin ac", "guaifenesin + codeine");
-//        drugData.put("HCTZ", "hydroclorothyazide");
-//        drugData.put("glucophage", "metformin");
-//        drugData.put("actos", "pioglitazone");
-//        drugData.put("tamiflu", "oseltamivir");
-//        drugData.put("synthroid", "levothyroxine");
-//        drugData.put("advil", "ibuprofen");
-//        drugData.put("mobic", "meloxicam");
-//        drugData.put("celebrex", "celecoxib");
-//        drugData.put("augmentin", "amoxicillin + clavulanate");
-//        drugData.put("lipitor", "atorvastatin");
-//        drugData.put("zocor", "simvastatin");
-//        drugData.put("crestor", "rosuvastatin");
-//        drugData.put("vytorin", "ezetimibe + simvastatin");
-//        drugData.put("prinivil", "lisinopril");
-//        drugData.put("altace", "ramipril");
-//        drugData.put("vasotec", "enalapril");
-//        drugData.put("vicotin", "hydrocodone");
-//        drugData.put("oxycotin", "oxycodone");
-//        drugData.put("ultram", "tramadol");
-//        drugData.put("tylenol3", "codeine + APAP");
+    private void initDrugData() {
 
         Scanner scanner = new Scanner(getResources().openRawResource(R.raw.druglist));
 
@@ -132,34 +55,98 @@ public class MainActivity extends AppCompatActivity {
             String line = scanner.nextLine();
             String[] data = line.split("\t");
             if (data.length >= 2) {
-                drugData.put(data[0],data[1]);
+                brandAndGeneric.put(data[0],data[1]);
             }
         }
 
         scanner.close();
-
-        Log.d("drug list",drugData.toString());
-
-        return drugData;
     }
 
     /**
-     * Generates random drugs list to appear on the screen
-     *
-     * @return the map of the random key, value drugs'name and its generic
+     * Generates a list of random generic and a random brand name to appear on the screen
      */
-    private Map<String,String> generateListDrugs() {
-        Map<String,String> generatedDrugs = new HashMap<>();
-        Map<String, String> brandAndGeneric = initDrugData();
+    private void generateListDrugs() {
+        Random randNo = new Random();
 
         while (generatedDrugs.size() != NO_GENERIC_ON_SCREEN) {
-            Random randNo = new Random();
             int randNum = randNo.nextInt(brandAndGeneric.size());
             generatedDrugs.put((String) brandAndGeneric.keySet().toArray()[randNum],
                     (String) brandAndGeneric.values().toArray()[randNum]);
         }
 
-        Log.d("generatedDrugs",generatedDrugs.toString());
-        return generatedDrugs;
+        /* Generates a random number from the set of drug name */
+        brandName = (String) generatedDrugs.keySet().toArray()[randNo.nextInt(NO_GENERIC_ON_SCREEN)];
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("onResume", "onResume start!");
+
+        display();
+        pickDrugName();
+        Log.d("generatedBradOnResume",brandName);
+    }
+
+    /**
+     * Displays brand name and list of generic on the screen
+     */
+    protected void display() {
+        TextView genericView = (TextView) findViewById(R.id.brand);
+
+        genericView.setText(brandName);
+
+        /* Displays a list of random generic name on the screen */
+        ArrayAdapter<String> drugAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, generatedDrugs.values().toArray(new String[0]));
+        drugView.setAdapter(drugAdapter);
+    }
+
+    /**
+     * Gives feedback right or wrong when the user clicks on the generic name on the list
+     */
+    protected void pickDrugName() {
+        drugView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int brand, long l) {
+                Log.d("position", "the user click on: " + brand);
+                Log.d("position", "the user click on: " + generatedDrugs.values().toArray()[brand]);
+                Log.d("position", "the user click on: " + generatedDrugs.get(brandName));
+
+                if (generatedDrugs.get(brandName).equals(generatedDrugs.values().toArray()[brand])) {
+                    Toast.makeText(MainActivity.this, "You are awesome!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "You suck!", Toast.LENGTH_SHORT).show();
+                }
+                generateListDrugs();
+                display();
+                pickDrugName();
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("brandName", brandName);
+        outState.putSerializable("generatedDrugs", (Serializable) generatedDrugs);
+
+        Log.d("generatedBradSave",brandName);
+        Log.d("generatedDrugsSave",generatedDrugs.toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        if (savedInstanceState.containsKey("brandName") && savedInstanceState.containsKey("generatedDrugs")) {
+            brandName = savedInstanceState.getString("brandName");
+            generatedDrugs = (HashMap<String, String>) savedInstanceState.getSerializable("generatedDrugs");
+            Log.d("onRestoreInstanceState", "onRestoreInstanceState start!");
+        }
+        Log.d("generatedBradRestore",brandName);
+        Log.d("generatedDrugsRestore",generatedDrugs.toString());
+    }
+
 }

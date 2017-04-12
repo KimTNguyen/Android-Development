7a9189d1ef983f70b54a20c79f29636a4775ffeb
KimTNguyen <kimthy1234@yahoo.com>
2017-04-12 12:44:41 -0700

    using RecyclerView to display list of drugs

--- DrugList/app/src/main/java/projects/android/nguyen/kim/pharmacyTechPractice/controller/DrugListFragment.java
+++ DrugList/app/src/main/java/projects/android/nguyen/kim/pharmacyTechPractice/controller/DrugListFragment.java
@@ -3,6 +3,8 @@ package projects.android.nguyen.kim.pharmacyTechPractice.controller;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,11 +19,16 @@ import projects.android.nguyen.kim.pharmacyTechPractice.R;
  */
 public class DrugListFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_drug_list, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_drug_list, container, false);
 
+        recyclerView = (RecyclerView) view.findViewById(R.id.drug_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
 }
